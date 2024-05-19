9dd6ca56ee021f36c9c4c2b78ab0299d00058cdc
SURFACEPRO9\mitsu <mitsuhiro.seino@gmail.com>
2024-05-19 12:11:36 +0900

    line/bot-sdk

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+const config = {
+  channelAccessToken: '',
+  channelSecret: '',
+};
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import send from './send';
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+  await send();
   return {
     statusCode: 200,
     body: 'OK!',
